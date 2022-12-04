7c071e2e3d2fd78b1c6c9e75f9b5f77f6594ac7f
daniel-buritica <buritica_1999@hotmail.com>
2022-12-04 04:57:08 -0500

    update

--- spring-boot-reactor/src/main/java/com/bolsasdeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/bolsasdeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -9,10 +9,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootReactorApplication.class, args);
     }
@@ -20,21 +24,36 @@ public class SpringBootReactorApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Flux<String> nombres =  Flux.just("nombre1","","nombre2","nombre3")
-                .doOnNext(e -> {
-                    if (e.isEmpty()){
-                        new RuntimeException("Nombres no vacios");
+        ejmploEterable();
+    }
+
+    public void ejmploEterable() throws Exception {
+        List<String> usuariosList = new ArrayList<>();
+        usuariosList.add("daniel buritica");
+        usuariosList.add("sonia junco");
+        usuariosList.add("blanca triana");
+        usuariosList.add("steven buritica");
+        usuariosList.add("steven buritica");
+
+        Flux<String> nombres = Flux.fromIterable(usuariosList);/*Flux.just("daniel buritica", "sonia junco", "blanca triana", "steven buritica", "steven buritica");*/
+
+        Flux<Usuario> usuarios = nombres
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .filter(usuario -> usuario.getApellido().equalsIgnoreCase("triana"))
+                .doOnNext(usuario -> {
+                    if (usuario == null) {
+                        throw new RuntimeException("Nombres no pueden ir vacios");
                     }
-                    System.out.println(e);
+                    System.out.println(usuario.getNombre());
+                })
+                .map(usuario -> {
+                    usuario.setNombre(usuario.getNombre().toUpperCase());
+                    return usuario;
                 });
-        nombres.subscribe(e -> log.info(e)
-                , error -> log.error(error.getMessage())
-                , new Runnable() {
-                    @Override
-                    public void run() {
-                        log.info("Ha terminado el flujo");
-                    }
-                });
+
+        usuarios.subscribe(e -> log.info(e.toString()),
+                error -> log.error(error.getMessage()));
+
 
     }
 }
---
+++ spring-boot-reactor/src/main/java/com/bolsasdeideas/springboot/reactor/app/Usuario.java
@@ -0,0 +1,15 @@
+package com.bolsasdeideas.springboot.reactor.app;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    private String nombre;
+    private String apellido;
+}
